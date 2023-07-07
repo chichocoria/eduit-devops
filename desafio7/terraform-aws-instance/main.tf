025ae5b4d549f2f9bcfbd29d7a119dfe85786944
chichocoria <rubencoria7@gmail.com>
2023-07-07 13:24:17 -0300

    desafio 7 terminado

--- desafio7/terraform-aws-instance/main.tf
+++ desafio7/terraform-aws-instance/main.tf
@@ -4,11 +4,12 @@ resource "aws_key_pair" "key" {
 }
 
 resource "aws_default_vpc" "default_vpc" {
+
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow ssh inbound traffic"
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow ssh and http inbound traffic"
 
   # using default VPC
   vpc_id      = aws_default_vpc.default_vpc.id
@@ -26,8 +27,27 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "TLS from VPC"
+
+    # we should allow incoming and outoging
+    # TCP packets
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    # allow all traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "allow_ssh"
+    Name = "allow_ssh_http"
   }
 }
 
@@ -35,7 +55,7 @@ resource "aws_instance" "example_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name = aws_key_pair.key.key_name
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups = [aws_security_group.allow_ssh_http.name]
   tags = {
     Name = "eduit-example"
   }
